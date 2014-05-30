7403ae0b36e87bfdba40e3bfc6e0c95c4ac04a85
Joshua Howland <development@jkhowland.com>
2014-05-30 03:02:15 -0600

    Initial Commit

---
+++ DayXTests/DayXTests.m
@@ -0,0 +1,34 @@
+//
+//  DayXTests.m
+//  DayXTests
+//
+//  Created by Joshua Howland on 5/30/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DayXTests : XCTestCase
+
+@end
+
+@implementation DayXTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
