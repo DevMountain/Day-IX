54ed7917684f2abdd6cde36c512f95b91d1be7c3
Joshua Howland <development@jkhowland.com>
2014-09-15 12:10:12 -0600

    Adds text field to view controller and sets as delegate

--- DayX/DXDetailViewController.m
+++ DayX/DXDetailViewController.m
@@ -8,7 +8,9 @@
 
 #import "DXDetailViewController.h"
 
-@interface DXDetailViewController ()
+@interface DXDetailViewController () <UITextFieldDelegate>
+
+@property (nonatomic, strong) IBOutlet UITextField *textField;
 
 @end
 
@@ -16,22 +18,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+
+    self.textField.delegate = self;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return YES;
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
