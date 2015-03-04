11d9b3dd93bb54ca108927976a79153067a3d96e
Ben Norris <ben@bsn.io>
2015-03-03 20:02:12 -0700

    Adds Apple Watch setup and icons

---
+++ DayX WatchKit Extension/GlanceController.h
@@ -0,0 +1,14 @@
+//
+//  GlanceController.h
+//  DayX WatchKit Extension
+//
+//  Created by Ben Norris on 3/3/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface GlanceController : WKInterfaceController
+
+@end
---
+++ DayX WatchKit Extension/InterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  InterfaceController.m
+//  DayX WatchKit Extension
+//
+//  Created by Ben Norris on 3/3/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController()
+
+@end
+
+
+@implementation InterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
